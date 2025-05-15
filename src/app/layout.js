e1c6385dfd1ce419f1385a96c3d5cbd7f37e2ff6
jungho <kiik52@naver.com>
2025-05-15 14:13:16 +0900

    - 헤더 폴더 구조 변경 및 메뉴데이터 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
+import Header from "./components/header/Header";
 import Footer from "./components/Footer";
 
 export const metadata = {
