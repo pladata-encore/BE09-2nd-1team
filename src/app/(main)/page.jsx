70d87d94a732753acbaff5253944ef28e1ff2a3c
jungho <kiik52@naver.com>
2025-05-16 17:34:39 +0900

    - 메인페이지 section04

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,6 +1,7 @@
 import Section01 from "@/components/main/Section01";
 import Section02 from "@/components/main/Section02";
 import Section03 from "@/components/main/Section03";
+import Section04 from "@/components/main/Section04";
 import React from "react";
 
 export default function MainPage() {
@@ -10,6 +11,7 @@ export default function MainPage() {
         <Section01 />
         <Section02 />
         <Section03 />
+        <Section04 />
       </main>
     </div>
   );
