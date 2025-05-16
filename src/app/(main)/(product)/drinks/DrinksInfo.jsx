6c3c4b6f932e5d52b902011b8f51e0303ba7416a
heechae <chaehee15@naver.com>
2025-05-16 15:51:21 +0900

    사이트 애니메이션 제외 완료

--- src/app/(main)/(product)/drinks/DrinksInfo.jsx
+++ src/app/(main)/(product)/drinks/DrinksInfo.jsx
@@ -1,12 +1,25 @@
 "use client";
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import drinks from "./drinks.json";
+import { usePathname } from "next/navigation";
 import DrinksModal from "./DrinksModal";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function DrinksInfo() {
   const [hoveredIndex, setHoveredIndex] = useState(null);
   const [selectedDrink, setSelectedDrink] = useState(null);
   const hoverTimeout = useRef(null);
+  const pathname = usePathname();
+
+  useEffect(() => {
+      // AOS 초기화
+      AOS.init({
+        duration: 800,
+        once: true, // 스크롤 시 애니메이션이 한 번만 실행되도록 설정
+      });
+      AOS.refresh();
+    }, [pathname]);
 
   const handleMouseEnter = (index) => {
     if (hoverTimeout.current) {
@@ -32,7 +45,7 @@ export default function DrinksInfo() {
           alt="제품 소개"
           className="object-cover w-full h-full"
         />
-        <div className="absolute inset-0 flex items-center justify-center">
+        <div className="absolute inset-0 flex items-center justify-center" data-aos="fade-up">
           <span className="text-6xl font-bold text-white">
             커피/음료/디저트
           </span>
--- src/app/(main)/(product)/product/ProductInfo.jsx
+++ src/app/(main)/(product)/product/ProductInfo.jsx
@@ -1,15 +1,26 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { usePathname } from "next/navigation";
 import products from "./products.json";
 import ProductModal from "./ProductModal";
-import gsap from "gsap";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function ProductInfo() {
   const [hoveredIndex, setHoveredIndex] = useState(null);
   const [selectedProduct, setSelectedProduct] = useState(null);
   const hoverTimeout = useRef(null);
+  const pathname = usePathname();
+  
+  useEffect(() => {
+      // AOS 초기화
+      AOS.init({
+        duration: 800,
+        once: true, // 스크롤 시 애니메이션이 한 번만 실행되도록 설정
+      });
+      AOS.refresh();
+    }, [pathname]);
 
   const handleMouseEnter = (index) => {
     if (hoverTimeout.current) {
@@ -35,7 +46,7 @@ export default function ProductInfo() {
           alt="제품 소개"
           className="object-cover w-full h-full"
         />
-        <div className="absolute inset-0 flex items-center justify-center">
+        <div className="absolute inset-0 flex items-center justify-center" data-aos="fade-up">
           <span className="text-6xl font-bold text-white">
             제품소개
           </span>
@@ -49,7 +60,7 @@ export default function ProductInfo() {
             // 제품 카드
             <div
               key={index}
-              className={`mb-6 rounded-lg border bg-amber-50 transition-all duration-300 overflow-hidden relative`}
+              className="relative mb-6 overflow-hidden transition-all duration-300 border rounded-lg bg-amber-50"
               style={
                 isHovered
                   ? {
@@ -62,6 +73,7 @@ export default function ProductInfo() {
               onMouseEnter={() => handleMouseEnter(index)}
               onMouseLeave={handleMouseLeave}
               onClick={() => setSelectedProduct(product)}
+              data-aos="fade-left"
             >
               <h1
                 className={`mt-8 ml-5 text-3xl font-bold text-left 
@@ -77,7 +89,7 @@ export default function ProductInfo() {
               </h1>
               {/* 이미지와 설명을 flex로 감쌈 */}
               <div
-                className={`m-5 flex flex-col ${
+                className={`mt-5 md-5 flex flex-col ${
                   isHovered ? "justify-center items-center" : "items-start"
                 }`}
                 style={{ minHeight: "320px" }}
@@ -85,12 +97,12 @@ export default function ProductInfo() {
                 <img
                   src={isHovered ? product.imagedetail : product.image}
                   className={`transition-all duration-300 ${
-                    isHovered ? "w-3/4 h-3/4" : "w-full h-10/12"
+                    isHovered ? "w-3/4 h-full" : "w-full h-10/12"
                   }`}
                   alt={`${product.name} 이미지`}
                 />
                 {isHovered && (
-                  <p className="w-full mt-4 text-lg text-center text-white">
+                  <p className="w-full m-4 text-lg text-center text-white">
                     {product.description}
                   </p>
                 )}
@@ -99,7 +111,7 @@ export default function ProductInfo() {
           );
         })}
       </div>
-      {/* 제품 상세정보 모달달 */}
+      {/* 제품 상세정보 모달 */}
       <ProductModal
         product={selectedProduct}
         onClose={() => setSelectedProduct(null)}
