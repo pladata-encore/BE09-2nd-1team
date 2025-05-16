06ba173826f4260434778dbe6efa5d3ddf81aa6c
FerryLa <rlawl4240@gmail.com>
2025-05-16 09:11:59 +0900

    commit

--- src/app/(main)/(company)/greeting/page.jsx
+++ src/app/(main)/(company)/greeting/page.jsx
@@ -1,5 +1,34 @@
 import React from "react";
 
-export default function page() { 
-  return <>page</>;
+export default function page() {
+  return (
+    <>
+      <div className="relative w-full h-[500px]">
+        <img
+          src="/images/greeting/s_visual6.jpg"
+          alt="인사말"
+          className="object-cover w-full h-full"
+        />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <span className="text-6xl font-bold text-white">인사말</span>
+        </div>
+      </div>
+      <p></p>
+      <div>since 1957</div>
+
+      <div>
+        1957년 대구 남문시장에서 삼송제과로 시작된 삼송빵집은 3대를 이어 대구를
+        대표하는 추억과 역사의 빵집으로 많은 사랑을 받고 있습니다.
+      </div>
+
+      <div>
+        정직한 경영과 성실한 제품 개발이 국내 최초의 오븐에 구운 고로케와
+        통옥수수빵이 탄생과 성공으로 이어졌고 오늘날 한국을 대표하는 베이커리
+        브랜드 중 하나인 “삼송빵집”을 있게 했습니다. 삼송비엔씨는 대형
+        프랜차이즈와 경쟁을 이겨낸 경험과 긴 업력의 노하우를 바탕으로 2022년부터
+        가맹사업 전개, 생산시스템 확충, 해외 진출 등으로 더욱 단단한 성장의
+        발판을 준비하고 있습니다. 대표이사 박성욱
+      </div>
+    </>
+  );
 }
