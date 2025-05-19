f212bfa399fc2bc045c4c20adb5da8239817f705
jungho <kiik52@naver.com>
2025-05-19 12:06:11 +0900

    - 브랜드스토리 페이지 작업 완료

--- src/app/(main)/(brand)/story/page.jsx
+++ src/app/(main)/(brand)/story/page.jsx
@@ -1,6 +1,15 @@
 import Submenu from "@/components/subMenu/Submenu";
 import React from "react";
+import Section01 from "./components/Section01";
+import Section02 from "./components/Section02";
+import Section03 from "./components/Section03";
 
 export default function page() {
-  return <section className="min-h-screen">page</section>;
+  return (
+    <div className="w-full min-h-screen overflow-hidden text-base">
+      <Section01 />
+      <Section02 />
+      <Section03 />
+    </div>
+  );
 }
