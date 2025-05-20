15180317244272c47f523bd391b6c5ce4002d050
jungho <kiik52@naver.com>
2025-05-20 16:04:49 +0900

    .next 파일 삭제

--- src/app/(main)/(store)/samgsong-1957/page.jsx
+++ src/app/(main)/(store)/samgsong-1957/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Header from "@/components/header/Header";
 import Footer from "@/components/Footer";
 import HeroSection from "./components/HeroSection";
@@ -6,18 +6,17 @@ import KakaoMap from "@/components/KakaoMap";
 import VideoSection from "./components/VideoSection";
 import ProductMenuSection from "./components/ProductMenuSection";
 import StoreLocationSection from "./components/StoreLocationSection";
-
+import { Suspense } from "react";
 
 export default function page() {
   return (
     <main className="flex flex-col">
-
+      <Suspense fallback={<div>로딩 중...</div>}>
         <HeroSection />
         <VideoSection />
         <ProductMenuSection />
         <StoreLocationSection />
-
-    
+      </Suspense>
     </main>
-  )
+  );
 }
